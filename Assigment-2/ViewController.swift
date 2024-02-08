a6efecb70426ab95ac91914b21ef2c5c37ac2544
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2024-02-08 13:56:17 +0530

    first Commit

---
+++ Assigment-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigment-2
+//
+//  Created by Ravindrasinh Medhat on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
